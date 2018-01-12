8404e2702302546f3edfcbd3f73b6b9287bad756
tianlan <tianlantianlan95@gmail.com>
2018-01-11 22:59:48 -0600

    add jar

--- server/src/TeamModel.java
+++ server/src/TeamModel.java
@@ -1,3 +1,18 @@
+import javafx.scene.canvas.GraphicsContext;
 
 public class TeamModel extends Model {
+    @Override
+    public void draw(GraphicsContext gc) {
+
+    }
+
+    @Override
+    public void reqUpdate(WorldObject u) {
+
+    }
+
+    @Override
+    WorldClock getTimer() {
+        return null;
+    }
 }
