f92bc6210a7fa7dd2f1ca94a62fb93fd25524126
Avery Whitaker <averywhitaker@gmail.com>
2018-01-12 14:28:30 -0600

    Prototyped Random map generation complete - missed some files

---
+++ common/src/TileStone.java
@@ -0,0 +1,14 @@
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+
+public class TileStone extends TileTest {
+
+    public TileStone(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public Paint getColor() {
+        return Color.BLUEVIOLET;
+    }
+}
