8e81043b2cc46d4252c163e373f8ded179c7ee3d
ryanhan <ryanhan32597@gmail.com>
2018-01-18 17:44:45 -0600

    Cell shape stuff.

--- game/src/game/model/Game/WorldObject/Shape/CellShape.java
+++ game/src/game/model/Game/WorldObject/Shape/CellShape.java
@@ -7,18 +7,29 @@ import javafx.scene.canvas.GraphicsContext;
 import java.util.Collections;
 import java.util.Set;
 
-/*
-A shape that always take up an entire cell (for blockers)
+/**
+ * A shape that always takes up an entire cell, for blocking an entire cell.
  */
 public class CellShape extends Shape {
 
+    /**
+     * The cell reference that this shape is in.
+     */
     private GridCellReference gridCell;
 
+    /**
+     * Constructor of the shape with given coordinates.
+     * @param x x-coordinate of the upper left corner of the cell
+     * @param y y-coordinate of the upper left corner of the cell
+     */
     public CellShape(int x, int y) {
         this.gridCell = new GridCellReference(x, y);
     }
-    public CellShape() {
 
+    /**
+     * Default constructor needed for serialization.
+     */
+    public CellShape() {
     }
 
     @Override
@@ -27,9 +38,7 @@ public class CellShape extends Shape {
     }
 
     @Override
-    public double getY() {
-        return gridCell.y + 0.5;
-    }
+    public double getY() { return gridCell.y + 0.5; }
 
     @Override
     public boolean blocksCell(int x, int y) {
@@ -48,29 +57,30 @@ public class CellShape extends Shape {
 
     @Override
     public void shift(double x, double y) {
+        if ((x - Math.round(x)) > 0.01 || (y - Math.round(y)) > 0.01)
+            throw new RuntimeException("Cell shape cannot shift by a non-integer value.");
+
         this.gridCell.x += Math.round(x);
         this.gridCell.y += Math.round(y);
-
-        if ((x - Math.round(x)) > 0.01 || (y - Math.round(y)) > 0.01)
-            throw new RuntimeException("Cell shape cannot move by non-integer");
     }
 
     @Override
     public void setPos(double x, double y) {
+        if ((x - Math.round(x)) > 0.01 || (y - Math.round(y)) > 0.01)
+            throw new RuntimeException("Cell shape position cannot be set to a non-integer value.");
+
         this.gridCell.x = (int) Math.round(x);
         this.gridCell.y = (int) Math.round(y);
-        if ((x - Math.round(x)) > 0.01 || (y - Math.round(y)) > 0.01)
-            throw new RuntimeException("Cell shape cannot be set to non-integer");
     }
 
     @Override
     public void rotate(double r) {
-        throw new RuntimeException("Cell shape cannot rotate");
+        throw new RuntimeException("Cell shape cannot rotate.");
     }
 
     @Override
     public void setAngle(double r) {
-        throw new RuntimeException("Cell shape cannot set angle");
+        throw new RuntimeException("Cell shape cannot set angle.");
     }
 
     @Override
