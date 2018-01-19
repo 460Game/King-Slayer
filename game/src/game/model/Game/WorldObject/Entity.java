a47c8dfdedcfc11777243ed59d8015d2df87990b
ryanhan <ryanhan32597@gmail.com>
2018-01-18 22:39:52 -0600

    Entity

--- game/src/game/model/Game/WorldObject/Entity.java
+++ game/src/game/model/Game/WorldObject/Entity.java
@@ -27,7 +27,7 @@ public abstract class Entity {
     private double speed;
 
     /**
-     * Current angle with which the angle is moving.
+     * Current angle with which the entity is moving.
      */
     private double angle;
 
@@ -36,30 +36,138 @@ public abstract class Entity {
      */
     private Team team;
 
+    /**
+     * ID of this entity.
+     */
     private long id = Util.random.nextLong();
+
+    /**
+     * Time of the last update this entity performed.
+     */
     private long last_update;
 
-    public abstract void collision(GameModel model, Entity collidesWith);
-
-    public void copyOf(Entity other) {
-        this.id = other.id;
-        this.last_update = other.last_update;
-    }
-
-
-    public abstract Shape getShape();
-
-    public long getId() {
-        return id;
-    }
-
+    /**
+     * Default constructor needed for serialization.
+     */
     Entity() {
+
     }
+
+    /**
+     * Constructor for the entity with the given game model
+     * @param model current game model
+     */
     Entity(GameModel model) {
         last_update = model.nanoTime();
     }
 
+
+    /**
+     * Gets the current health of the entity.
+     * @return the current health of the entity
+     */
+    public int getHealth() {
+        return health;
+    }
+
+    /**
+     * Sets the current health of the entity to the amount specified.
+     * @param health new health of the entity
+     */
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    /**
+     * Gets the current speed of the entity.
+     * @return the current speed of the entity
+     */
+    public double getSpeed() {
+        return speed;
+    }
+
+    /**
+     * Sets the current speed of the entity to the amount specified.
+     * @param speed new speed of the entity
+     */
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    /**
+     * Gets the current angle of movement of the entity.
+     * @return the current angle of movement of the entity
+     */
+    public double getMovementAngle() {
+        return angle;
+    }
+
+    /**
+     * Sets the current angle of movement of the entity to the angle
+     * specified.
+     * @param angle new angle of movement
+     */
+    public void setMovementAngle(double angle) {
+        this.angle = angle;
+    }
+
+    /**
+     * Gets the current health of the entity.
+     * @return the current health of the entity
+     */
+    public Team getTeam() {
+        return team;
+    }
+
+    /**
+     * Sets the team of the entity to the team specified
+     * @param team new team of the entity
+     */
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    /**
+     * Perform collision with the other entity in the current game model.
+     * @param model current game model
+     * @param collidesWith entity that this entity collides with
+     */
+    public abstract void collision(GameModel model, Entity collidesWith);
+
+    /**
+     * Sets this entity to be a copy of another entity.
+     * @param other the entity to be copied
+     */
+    public void copyOf(Entity other) {
+        this.health = other.health;
+        this.angle = other.angle;
+        this.speed = other.speed;
+        this.team = other.team;
+        this.id = other.id;
+        this.last_update = other.last_update;
+    }
+
+    /**
+     * Gets the shape of the entity.
+     * @return the shape of the entity
+     */
+    public abstract Shape getShape();
+
+    /**
+     * Gets the id of the entity.
+     * @return the id of the entity
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * Updates the entity in the game model.
+     * @param time time since the last update
+     * @param model current game model
+     */
     public abstract void update(long time, GameModel model);
+
 
     public void update(GameModel model) {
 
