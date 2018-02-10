9f32523474461ee272192604d4509a2950dc752e
Madison Lewis <empl.lewis@gmail.com>
2018-02-09 21:04:49 -0600

    Images for players show up, movement doesn't work

--- kingslayer/src/game/model/game/model/worldObject/entity/drawStrat/DirectionAnimationDrawStrat.java
+++ kingslayer/src/game/model/game/model/worldObject/entity/drawStrat/DirectionAnimationDrawStrat.java
@@ -6,14 +6,48 @@ import images.Images;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 import static util.Util.toDrawCoords;
 
 public abstract class DirectionAnimationDrawStrat extends DrawStrat {
+
+  public DrawData drawData = DrawData.makeAnimated();
+  public int height = 32;
+  public int width = 32;
 
   public static final DirectionAnimationDrawStrat RED_KING_ANIMATION = new RedKingDirectionAnimationDrawStrat();
   public static final DirectionAnimationDrawStrat BLUE_KING_ANIMATION = new BlueKingDirectionAnimationDrawStrat();
   public static final DirectionAnimationDrawStrat RED_SLAYER_ANIMATION = new RedSlayerDirectionAnimationDrawStrat();
   public static final DirectionAnimationDrawStrat BLUE_SLAYER_ANIMATION = new BlueSlayerDirectionAnimationDrawStrat();
+
+  /**
+   * Key for figuring out which piece of the character sheet is needed for
+   * the king at a given point in time.
+   */
+  private static Map<String, Point> imageMap = new HashMap<>();
+
+  static {
+    // Read in file detailing which images to use at which points in time in the animation
+    // of the king.
+    Scanner input;
+    try {
+      input = new Scanner(Images.class.getResource("players.txt").openStream());
+
+      while (input.hasNext()) {
+        Point curPoint = new Point(input.nextInt(), input.nextInt());
+        if (curPoint.x == -1) break;
+        input.nextLine();
+        String info = input.nextLine();
+        imageMap.put(info, curPoint);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
   @Override
   public DrawData initDrawData() {
@@ -23,10 +57,16 @@ public abstract class DirectionAnimationDrawStrat extends DrawStrat {
   abstract Image getImage();
 
   public void draw(Entity entity, GraphicsContext gc) {
-    gc.drawImage(getImage(), toDrawCoords(entity.data.x),
-        toDrawCoords(entity.data.y),
-        toDrawCoords(entity.data.hitbox.getWidth()),
-        toDrawCoords(entity.data.hitbox.getHeight()));
+    try {
+      Point p = imageMap.get(drawData.imageNum + "" + drawData.direction);
+      gc.drawImage(getImage(),
+          toDrawCoords(entity.data.x),
+          toDrawCoords(entity.data.y),
+          toDrawCoords(entity.data.hitbox.getWidth()),
+          toDrawCoords(entity.data.hitbox.getHeight()));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   public double getDrawZ(EntityData entity) {
@@ -36,25 +76,93 @@ public abstract class DirectionAnimationDrawStrat extends DrawStrat {
   private static class RedKingDirectionAnimationDrawStrat extends DirectionAnimationDrawStrat {
     @Override
     Image getImage() {
-      return Images.BOX_IMAGE;
+      return Images.RED_KING_IMAGE_SHEET;
+    }
+
+    public void draw(Entity entity, GraphicsContext gc) {
+      try {
+        Point p = imageMap.get(drawData.imageNum + "" + drawData.direction);
+        gc.drawImage(this.getImage(),
+            toDrawCoords(p.x),
+            toDrawCoords(p.y),
+            width,
+            height,
+            toDrawCoords(entity.data.x),
+            toDrawCoords(entity.data.y),
+            toDrawCoords(entity.data.hitbox.getWidth()),
+            toDrawCoords(entity.data.hitbox.getHeight()));
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     }
   }
   private static class BlueKingDirectionAnimationDrawStrat extends DirectionAnimationDrawStrat {
     @Override
     Image getImage() {
-      return Images.BOX_IMAGE;
+      return Images.BLUE_KING_IMAGE_SHEET;
+    }
+
+    public void draw(Entity entity, GraphicsContext gc) {
+      try {
+        Point p = imageMap.get(drawData.imageNum + "" + drawData.direction);
+        gc.drawImage(this.getImage(),
+            toDrawCoords(p.x),
+            toDrawCoords(p.y),
+            width,
+            height,
+            toDrawCoords(entity.data.x),
+            toDrawCoords(entity.data.y),
+            toDrawCoords(entity.data.hitbox.getWidth()),
+            toDrawCoords(entity.data.hitbox.getHeight()));
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     }
   }
   private static class RedSlayerDirectionAnimationDrawStrat extends DirectionAnimationDrawStrat {
     @Override
     Image getImage() {
-      return Images.BOX_IMAGE;
+      return Images.RED_SLAYER_IMAGE_SHEET;
+    }
+
+    public void draw(Entity entity, GraphicsContext gc) {
+      try {
+        Point p = imageMap.get(drawData.imageNum + "" + drawData.direction);
+        gc.drawImage(this.getImage(),
+            toDrawCoords(p.x),
+            toDrawCoords(p.y),
+            width,
+            height,
+            toDrawCoords(entity.data.x),
+            toDrawCoords(entity.data.y),
+            toDrawCoords(entity.data.hitbox.getWidth()),
+            toDrawCoords(entity.data.hitbox.getHeight()));
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     }
   }
   private static class BlueSlayerDirectionAnimationDrawStrat extends DirectionAnimationDrawStrat {
     @Override
     Image getImage() {
-      return Images.BOX_IMAGE;
+      return Images.BLUE_SLAYER_IMAGE_SHEET;
+    }
+
+    public void draw(Entity entity, GraphicsContext gc) {
+      try {
+        Point p = imageMap.get(drawData.imageNum + "" + drawData.direction);
+        gc.drawImage(this.getImage(),
+            toDrawCoords(p.x),
+            toDrawCoords(p.y),
+            width,
+            height,
+            toDrawCoords(entity.data.x),
+            toDrawCoords(entity.data.y),
+            toDrawCoords(entity.data.hitbox.getWidth()),
+            toDrawCoords(entity.data.hitbox.getHeight()));
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     }
   }
 
