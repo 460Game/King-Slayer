9c188763bcee1f6d70ead77ba7075ecd0f08144a
tianlan <tianlantianlan95@gmail.com>
2018-02-13 16:54:45 -0600

    4 player

--- kingslayer/src/network/RemoteConnection.java
+++ kingslayer/src/network/RemoteConnection.java
@@ -100,7 +100,7 @@ public class RemoteConnection {
                         messageQueues.put(connection.getID(), new LinkedBlockingQueue<>());
 
                         //TODO: might need to hard code here now, change it back!!!!!
-                        if (clientList.size() == 4) {
+                        if (clientList.size() == 3) {
                             adaptor.makeModel();
                         }
                     }
