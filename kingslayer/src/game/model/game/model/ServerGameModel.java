e8ed18526deff1d74c2f0b7bf6248f82acd02760
Madison Lewis <mpl7@rice.edu>
2018-02-14 16:57:06 -0600

    Both teams will get proper number of resources based on number of spawners

--- kingslayer/src/game/model/game/model/ServerGameModel.java
+++ kingslayer/src/game/model/game/model/ServerGameModel.java
@@ -141,18 +141,28 @@ public class ServerGameModel extends GameModel {
              //   }
 
             counter++;
-            int resources = 0;
-            for(Entity e: this.getAllEntities()) {
-                if (e.team != Team.NEUTRAL && e.role == Role.NEUTRAL) {
-                    resources++;
-                    if (counter > 500) {
-                        System.out.println("Team " + e.team + " has a resource spawner");
-                        changeResource(e.team, TeamResourceData.Resource.WOOD, 1);
-                        counter = 0;
+
+            if (counter > 500) {
+                int resourcesRed = 0;
+                for (Entity e : this.getAllEntities()) {
+                    if (e.team == Team.ONE && e.role == Role.NEUTRAL) {
+                        resourcesRed++;
                     }
                 }
+                System.out.println("Red: " + resourcesRed);
+                changeResource(Team.ONE, TeamResourceData.Resource.WOOD, resourcesRed);
+
+                int resourcesBlue = 0;
+                for (Entity e : this.getAllEntities()) {
+                    if (e.team == Team.TWO && e.role == Role.NEUTRAL) {
+                        resourcesBlue++;
+                    }
+                }
+                System.out.println("Blue: " + resourcesBlue);
+                changeResource(Team.TWO, TeamResourceData.Resource.WOOD, resourcesBlue);
+
+                counter = 0;
             }
-            //System.out.println("How much wood? " + resources);
 
             for(Model model : clients) {
                 model.processMessage(new UpdateResourceCommand(teamData.get(clientToTeamRoleMap.get(model).getKey()))); //TEMPORARY GARBAGE
