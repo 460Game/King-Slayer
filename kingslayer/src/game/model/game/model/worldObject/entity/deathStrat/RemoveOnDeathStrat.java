aaf5d2c08e01fc9c98313c5d961a170e527fb2e6
Madison Lewis <mpl7@rice.edu>
2018-02-19 13:32:40 -0600

    Commented out line that had errors and looked like it wasn't meant to be there anymore

---
+++ kingslayer/src/game/model/game/model/worldObject/entity/deathStrat/RemoveOnDeathStrat.java
@@ -0,0 +1,22 @@
+package game.model.game.model.worldObject.entity.deathStrat;
+
+import game.message.toClient.RemoveEntityCommand;
+import game.message.toClient.SetEntityCommand;
+import game.model.game.model.GameModel;
+import game.model.game.model.worldObject.entity.Entity;
+import game.model.game.model.worldObject.entity.collideStrat.CollisionStrat;
+
+public class RemoveOnDeathStrat extends DeathStrat {
+    public static final DeathStrat SINGLETON = new RemoveOnDeathStrat();
+
+    @Override
+    public void handleDeath(GameModel model, Entity entity) {
+        model.execute(serverGameModel ->
+            serverGameModel.processMessage(new RemoveEntityCommand(entity)), clientGameModel -> {});
+//            serverGameModel.removeByID(entity.id), clientGameModel ->  clientGameModel.removeByID(entity.id));
+        if (model.getAllEntities().contains(entity))
+            System.out.println("Still not removed");
+        else
+            System.out.println("REMOVED!!!!");
+    }
+}
