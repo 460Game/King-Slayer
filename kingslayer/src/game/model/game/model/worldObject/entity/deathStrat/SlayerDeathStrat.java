e6d2494ad8c463b586ccb859d88b1726aa90a08b
tianlan <tianlantianlan95@gmail.com>
2018-02-28 01:51:25 -0600

    slayer death handler

--- kingslayer/src/game/model/game/model/worldObject/entity/deathStrat/SlayerDeathStrat.java
+++ kingslayer/src/game/model/game/model/worldObject/entity/deathStrat/SlayerDeathStrat.java
@@ -1,12 +1,27 @@
 package game.model.game.model.worldObject.entity.deathStrat;
 
+import game.message.toServer.RemoveEntityRequest;
 import game.model.game.model.GameModel;
+import game.model.game.model.ServerGameModel;
+import game.model.game.model.team.Team;
 import game.model.game.model.worldObject.entity.Entity;
+
+import java.util.function.Consumer;
 
 public class SlayerDeathStrat extends DeathStrat {
     public final static SlayerDeathStrat SINGLETON = new SlayerDeathStrat();
     @Override
     public void handleDeath(GameModel model, Entity entity) {
+
         System.out.println("slayer dies");
+        Consumer<ServerGameModel> serverConsumer = (server) -> {
+            server.processMessage(new RemoveEntityRequest(entity));
+            server.teamWin((entity.getTeam() == Team.ONE) ? Team.TWO : Team.ONE);
+//            server.getClients().forEach(client -> client.processMessage(new SetEntityCommand(b)));
+        };
+        model.execute(serverConsumer, (client) -> {
+//            a.data.updateData.velocity.setMagnitude(0);
+//            client.removeByID(a.id);
+        });
     }
 }
