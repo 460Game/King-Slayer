479c4a0593d2c0a3d9dd4e3e7fd7bd497f92c89c
Madison Lewis <mpl7@rice.edu>
2018-03-01 11:04:13 -0600

    Minimal UI for Slayer

--- kingslayer/src/game/view/SlayerActionPanel.java
+++ kingslayer/src/game/view/SlayerActionPanel.java
@@ -1,10 +1,20 @@
 package game.view;
 
 import game.model.game.model.ClientGameModel;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 public class SlayerActionPanel extends ActionPanel {
 
     public SlayerActionPanel(ClientGameModel model, SlayerGameInteractionLayer gameInteractionLayer) {
         super(model); //supermodel
+
+        Text text = new Text("Left click to charge\nRight click to shoot");
+        text.setLayoutX(15);
+        text.setLayoutY(25);
+        text.setFont(new Font(20));
+        text.setFill(Color.WHITE);
+        this.getChildren().add(text);
     }
 }
