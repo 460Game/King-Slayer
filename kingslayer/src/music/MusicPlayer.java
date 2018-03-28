b9f9ee13875e024088a26d315fd05badc7ff9c7a
Madison Lewis <mpl7@rice.edu>
2018-03-28 16:00:06 -0500

    Added music back in, now works in the jar file format

--- kingslayer/src/music/MusicPlayer.java
+++ kingslayer/src/music/MusicPlayer.java
@@ -9,6 +9,7 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -28,138 +29,128 @@ public class MusicPlayer {
 
   static {
     try {
-      /*gameClip = AudioSystem.getClip();
-      AudioInputStream inputStream = AudioSystem.getAudioInputStream(MusicPlayer.class.getResourceAsStream("PirateCrew.wav"));
-      gameClip.open(inputStream);
+      gameClip = AudioSystem.getClip();
+      InputStream audioSrc = MusicPlayer.class.getResourceAsStream("PirateCrew.wav");
+      InputStream bufferedIn = new BufferedInputStream(audioSrc);
+      AudioInputStream audioStream = AudioSystem.getAudioInputStream(bufferedIn);
+      gameClip.open(audioStream);
 
       intro = AudioSystem.getClip();
-      inputStream = AudioSystem.getAudioInputStream(MusicPlayer.class.getResourceAsStream("Quest.wav"));
-      intro.open(inputStream);
-
-      error = AudioSystem.getClip();
-      inputStream = AudioSystem.getAudioInputStream(MusicPlayer.class.getResourceAsStream("Error.wav"));
-      error.open(inputStream);
-
-      bow = AudioSystem.getClip();
-      inputStream = AudioSystem.getAudioInputStream(MusicPlayer.class.getResourceAsStream("Bow.wav"));
-      bow.open(inputStream);
-
-      charge = AudioSystem.getClip();
-      inputStream = AudioSystem.getAudioInputStream(MusicPlayer.class.getResourceAsStream("Swish.wav"));
-      charge.open(inputStream);
-
-      sell = AudioSystem.getClip();
-      inputStream = AudioSystem.getAudioInputStream(MusicPlayer.class.getResourceAsStream("Sell.wav"));
-      sell.open(inputStream);
-
-      construction = AudioSystem.getClip();
-      inputStream = AudioSystem.getAudioInputStream(MusicPlayer.class.getResourceAsStream("Construction.wav"));
-      construction.open(inputStream);*/
+      audioSrc = MusicPlayer.class.getResourceAsStream("Quest.wav");
+      bufferedIn = new BufferedInputStream(audioSrc);
+      audioStream = AudioSystem.getAudioInputStream(bufferedIn);
+      intro.open(audioStream);
     } catch (Exception e) {
       e.printStackTrace();
     }
   }
 
   public static synchronized void playIntroMusic() {
-    /*if (gameClip.isRunning())
+    if (gameClip.isRunning())
       gameClip.stop();
-    intro.setFramePosition(0);
-    music = new Thread(){
-      public void run() {
-        try {
-          intro.loop(Clip.LOOP_CONTINUOUSLY);
-        } catch (Exception e) {
-          System.err.println(e.getMessage());
-        }
+    music = new Thread(() -> {
+      try {
+        intro.setFramePosition(0);
+        intro.loop(Clip.LOOP_CONTINUOUSLY);
+      } catch (Exception e) {
+        System.err.println(e.getMessage());
       }
-    };
-    music.start();*/
+    });
+    music.start();
   }
 
   public static synchronized void playGameMusic() {
-    /*if (intro.isRunning())
+    if (intro.isRunning())
       intro.stop();
-    gameClip.setFramePosition(0);
-    music = new Thread(){
-      public void run() {
-        try {
-          gameClip.loop(Clip.LOOP_CONTINUOUSLY);
-        } catch (Exception e) {
-          System.err.println(e.getMessage());
-        }
+    music = new Thread(() -> {
+      try {
+        gameClip.setFramePosition(0);
+        gameClip.loop(Clip.LOOP_CONTINUOUSLY);
+      } catch (Exception e) {
+        System.err.println(e.getMessage());
       }
-    };
-    music.start();*/
+    });
+    music.start();
   }
 
   public static synchronized void playArrowSound() {
-    /*bow.setFramePosition(0);
-    music = new Thread(){
-      public void run() {
-        try {
-          bow.start();
-        } catch (Exception e) {
-          System.err.println(e.getMessage());
-        }
+    music = new Thread(() -> {
+      try {
+        bow = AudioSystem.getClip();
+        InputStream audioSrc = MusicPlayer.class.getResourceAsStream("Bow.wav");
+        InputStream bufferedIn = new BufferedInputStream(audioSrc);
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(bufferedIn);
+        bow.open(audioStream);
+        bow.start();
+      } catch (Exception e) {
+        System.err.println(e.getMessage());
       }
-    };
-    music.start();*/
+    });
+    music.start();
   }
 
   public static synchronized void playChargeSound() {
-    /*charge.setFramePosition(0);
-    music = new Thread(){
-      public void run() {
-        try {
-          charge.start();
-        } catch (Exception e) {
-          System.err.println(e.getMessage());
-        }
+    music = new Thread(() -> {
+      try {
+        charge = AudioSystem.getClip();
+        InputStream audioSrc = MusicPlayer.class.getResourceAsStream("Swish.wav");
+        InputStream bufferedIn = new BufferedInputStream(audioSrc);
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(bufferedIn);
+        charge.open(audioStream);
+        charge.start();
+      } catch (Exception e) {
+        System.err.println(e.getMessage());
       }
-    };
-    music.start();*/
+    });
+    music.start();
   }
 
   public static synchronized void playSellSound() {
-    /*sell.setFramePosition(0);
-    music = new Thread(){
-      public void run() {
-        try {
-          sell.start();
-        } catch (Exception e) {
-          System.err.println(e.getMessage());
-        }
+    music = new Thread(() -> {
+      try {
+        sell = AudioSystem.getClip();
+        InputStream audioSrc = MusicPlayer.class.getResourceAsStream("Sell.wav");
+        InputStream bufferedIn = new BufferedInputStream(audioSrc);
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(bufferedIn);
+        sell.open(audioStream);
+        sell.start();
+      } catch (Exception e) {
+        System.err.println(e.getMessage());
       }
-    };
-    music.start();*/
+    });
+    music.start();
   }
 
   public static synchronized void playConstructionSound() {
-    /*construction.setFramePosition(0);
-    music = new Thread(){
-      public void run() {
-        try {
-          construction.start();
-        } catch (Exception e) {
-          System.err.println(e.getMessage());
-        }
+    music = new Thread(() -> {
+      try {
+        construction = AudioSystem.getClip();
+        InputStream audioSrc = MusicPlayer.class.getResourceAsStream("Construction.wav");
+        InputStream bufferedIn = new BufferedInputStream(audioSrc);
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(bufferedIn);
+        construction.open(audioStream);
+        construction.start();
+      } catch (Exception e) {
+        System.err.println(e.getMessage());
       }
-    };
-    music.start();*/
+    });
+    music.start();
   }
 
   public static synchronized void playErrorSound() {
-    /*error.setFramePosition(0);
-    music = new Thread(){
-      public void run() {
-        try {
-          error.start();
-        } catch (Exception e) {
-          System.err.println(e.getMessage());
-        }
+    music = new Thread(() -> {
+      try {
+        error = AudioSystem.getClip();
+        InputStream audioSrc = MusicPlayer.class.getResourceAsStream("Error.wav");
+        InputStream bufferedIn = new BufferedInputStream(audioSrc);
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(bufferedIn);
+        error.open(audioStream);
+        error.start();
+      } catch (Exception e) {
+        System.err.println(e.getMessage());
       }
-    };
-    music.start();*/
+    });
+    music.start();
   }
 
 }
