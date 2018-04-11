76117f8ffdba36da5efecc14b6c92ff9451cc67a
Madison Lewis <mpl7@rice.edu>
2018-04-11 11:31:09 -0500

    Better Slayer UI

--- kingslayer/src/game/view/SlayerActionPanel.java
+++ kingslayer/src/game/view/SlayerActionPanel.java
@@ -1,9 +1,11 @@
 package game.view;
 
+import com.sun.deploy.panel.AdvancedPanel;
 import game.model.game.model.ClientGameModel;
 import game.model.game.model.team.Team;
 import images.Images;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
@@ -25,20 +27,24 @@ public class SlayerActionPanel extends ActionPanel {
         GridPane grid = new GridPane();
         grid.setLayoutX(10);
         grid.setLayoutY(10);
-        grid.setHgap(5);
+        grid.setHgap(0);
         grid.setVgap(5);
 
         Text controls = new Text("Controls:");
-        if (model.getLocalPlayer().getTeam() == Team.BLUE_TEAM)
-            controls.setFill(Color.WHITE);
+        //if (model.getLocalPlayer().getTeam() == Team.BLUE_TEAM)
+        controls.setFill(Color.WHITE);
         grid.add(controls, 0, 0);
 
-        Button shoot = new Button("Shoot arrow", new ImageView(Images.SHOOTING_SYMBOL_IMAGE));
-        shoot.setStyle("-fx-background-color: #FFFFFFAA; ");
+        Label shoot = new Label("Shoot arrow   ");
+        shoot.setGraphic(new ImageView(Images.SHOOTING_SYMBOL_IMAGE));
+        shoot.setStyle("-fx-background-color: #ffffffaa;");
+        //shoot.setStyle("-fx-border-color: #aaaaaa; -fx-border-width: 2px;");
         grid.add(shoot, 1, 1);
 
-        Button charge = new Button("Charge        ", new ImageView(Images.CHARGING_SYMBOL_IMAGE));
-        charge.setStyle("-fx-background-color: #FFFFFFAA; ");
+        Label charge = new Label("Charge           ");
+        charge.setGraphic(new ImageView(Images.CHARGING_SYMBOL_IMAGE));
+        charge.setStyle("-fx-background-color: #ffffffaa;");
+        //charge.setStyle("-fx-border-color: #ffffffaa; -fx-border-width: 2px;");
         grid.add(charge, 0, 1);
 
         this.getChildren().add(grid);
